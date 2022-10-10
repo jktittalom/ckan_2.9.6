0e07351dc08f3fd2f35c2ae76277af06299327c2
jktittalom <jktittalom@gmail.com>
2022-10-10 18:17:50 +0530

    fixing app.py

--- contrib/docker/terria_master_json/jiten_terria_master/app.py
+++ contrib/docker/terria_master_json/jiten_terria_master/app.py
@@ -4,8 +4,11 @@ app = Flask('Terria Master JSON WebServer')
 @app.route('/')
 def terriamasterjson():
 	print("Welcome Jiten!!!")
-    return "ok Jiten"
+	return "ok"
 
+@app.route('/hello')
+def hello_world():
+   return "hello world"
 
 @app.route('/test')
 def test():
