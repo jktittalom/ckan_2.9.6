9486f98232c2111734d3fe73732e80485302aafa
jktittalom <jktittalom@gmail.com>
2022-10-10 19:47:50 +0530

    fixing app.py

--- contrib/docker/terria_master_json/jiten_terria_master/app.py
+++ contrib/docker/terria_master_json/jiten_terria_master/app.py
@@ -1,7 +1,3 @@
-from pathlib import Path
-import json
-import os
-
 from flask import Flask
 app = Flask('Terria Master JSON WebServer')
 
@@ -9,7 +5,6 @@ app = Flask('Terria Master JSON WebServer')
 def hello_world():
    return "Welcome Jiten!!"
 
-@app.route('/test')
+@app.route('/hello')
 def hello_world():
-	print("Testing indentaion!!!")
-    return "hello world"
+   return "hello world"
