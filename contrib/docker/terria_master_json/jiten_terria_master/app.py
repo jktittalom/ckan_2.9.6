bc8d747a93d474cda4e49e403e1cb206ac6e6ff6
jktittalom <jktittalom@gmail.com>
2022-10-10 16:59:40 +0530

    adding terria master container with port 5001

--- contrib/docker/terria_master_json/jiten_terria_master/app.py
+++ contrib/docker/terria_master_json/jiten_terria_master/app.py
@@ -4,3 +4,8 @@ app = Flask('Terria Master JSON WebServer')
 @app.route('/')
 def terriamasterjson():
     return [1,2,3]
+
+@app.route('/test')
+def test():
+	print("Welcome Jiten!!!")
+    return [1,2,3]
