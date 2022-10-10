7b8a1d7da794a742d04071eaed74532b0fbc4ffa
jktittalom <jktittalom@gmail.com>
2022-10-10 18:24:43 +0530

    fixing app.py

--- contrib/docker/terria_master_json/jiten_terria_master/app.py
+++ contrib/docker/terria_master_json/jiten_terria_master/app.py
@@ -1,11 +1,15 @@
+from pathlib import Path
+import json
+import os
+
 from flask import Flask
 app = Flask('Terria Master JSON WebServer')
 
 @app.route('/')
-def terriamasterjson():
-	print("Welcome Jiten!!!")
-	return "ok"
-
-@app.route('/hello')
 def hello_world():
+   return "Welcome Jiten!!"
+
+@app.route('/test')
+def hello_world():
+	print("Testing indentaion!!!")
    return "hello world"
